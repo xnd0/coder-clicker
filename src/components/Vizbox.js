fe18f9641f51eefd815ceddf22e82b442b96e512
xndcmd <xndcmd@gmail.com>
2022-10-18 07:57:13 -0700

    added visualizer component to app

---
+++ src/components/Vizbox.js
@@ -0,0 +1,13 @@
+export default function Vizbox() {
+
+    return (
+        <div className="vizbox">
+
+            <div>
+                Vizbox component
+            </div>
+
+        </div>
+    )
+
+}
